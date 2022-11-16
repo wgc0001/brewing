1c6b7abe352a14fcbcc72de63a4b8e986ea9e229
udigcowan <will.cowan@udig.com>
2022-11-15 19:47:57 -0600

    Update Controller

--- Controllers/RecipeController.cs
+++ Controllers/RecipeController.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-using static brewing.Models.Recipe;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,16 +5,6 @@ using System.Web;
 
 
 namespace BrewingApp.Controllers {
-    public class RecipeController: Controller {
-        public IActionResult Index() {
-            return View();
-        }
+    
 
-        public async Task<ActionResult<Recipe>> AddRecipe(Recipe recipe) {
-            try {
-                if (recipe ==null)
-                    return BadRequest()
-            }
-        }
-    }
 }
