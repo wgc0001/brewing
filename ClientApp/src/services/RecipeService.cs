ca421088c8991a9320dc10abdcd1e39fd536175f
udigcowan <will.cowan@udig.com>
2022-11-15 20:37:37 -0600

    initial recipe service commit

---
+++ ClientApp/src/services/RecipeService.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using AutoMapper;
+using brewing.Models;
+
+namespace RecipeService {
+    public interface IRecipeService {
+
+    }
+
+    public class RecipeService: IRecipeService {
+
+    }
+}
+
